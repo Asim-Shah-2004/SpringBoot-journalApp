9370fd204dbfa6e75ee6de5d7cc110900b728ec6
asim shah <asimshah2111@gmail.com>
2025-03-01 01:59:02 +0530

    used beans

---
+++ Using-Beans/src/main/java/net/project/journalApp/bean.java
@@ -0,0 +1,10 @@
+package net.project.journalApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class bean {
+    public String print(){
+        return "Hello From Bean";
+    }
+}
