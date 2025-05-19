28bc3d385b547bdba9fdd3734c9ed662d8cc9dc2
asim shah <asimshah2111@gmail.com>
2025-05-20 03:57:16 +0530

    added lombok

--- src/main/java/net/journal/journalApp/entity/JournalEntity.java
+++ src/main/java/net/journal/journalApp/entity/JournalEntity.java
@@ -5,9 +5,13 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.bson.types.ObjectId;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
 import javax.validation.constraints.NotBlank;
 
 @Document(collection = "journal")
+@Data
 public class JournalEntity {
 
     @Id
@@ -21,56 +25,5 @@ public class JournalEntity {
 
     private Instant createdAt;
     private Instant updatedAt;
-
-    // Default constructor for JSON deserialization
-    public JournalEntity() {
-    }
-
-    // Constructor with fields
-    public JournalEntity(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    @JsonIgnore
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Instant getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Instant updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 
 }
